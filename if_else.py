1d25b02963ddf5b10a3e7b0299334865d95ecee5
Priyanka Dangwal <84230110+PriyankaDangawal@users.noreply.github.com>
2024-07-08 05:47:47 +0530

    Add files via upload

---
+++ if_else.py
@@ -0,0 +1,44 @@
+# my calculetor
+
+# num_1=int(input("enter the first number: "))
+# num_2=int(input("enter the second number: "))
+# operator=input("enter the first operator: ")
+# print("Thank you for using our calculator")
+# print("please choose what operaton you want to perform")
+# if operator=="+":
+#     print(num_1+num_2)
+# elif operator=="-":
+#     print(num_1-num_2)
+# elif operator=="*":
+#     print(num_1*num_2)
+# elif operator=="/":
+#     print(num_1/num_2)
+# elif operator=="//":
+#     print(num_1//num_2)
+# elif operator=="**":
+#     print(num_1**num_2)
+# elif operator=="%":
+#     print(num_1%num_2)
+# else:
+#     print("please use velid operator to perform your opearton")
+
+
+# age=int(input("enter the age: "))
+# if age>=25:
+#     print("go to school")
+# elif age>=21:
+#     print("you can drive")
+# elif age>=18:
+#     print("You are eligible for marry")
+# else:
+#     print("please enter a velid age")
+
+
+
+
+# num=int(input("enter the number: "))
+# if num%2==0:
+#     print("enter number is even")
+# else:
+#     print("the number is odd")
+
---
+++ loop.py
@@ -0,0 +1,12 @@
+# num=input("enter num")
+# num2=int(num)
+# count=0
+# i=1
+# while i<=num2:
+#     num3=num2//10
+#     count+=1
+#     num2=num3
+# print(count)
+
+
+
